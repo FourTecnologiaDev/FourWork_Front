bb529c6c521a1c2534fdf326143ab25de40142f6
Pedro TortoriellO <pedrooofreitas@gmail.com>
2024-05-10 13:02:33 -0400

    10/05

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+// Porta que deseja usar durante a execução local (desenvolvimento)
+const devPort = 3000;
+
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: devPort
+  }
+});
